7f0e11b3d0366698893d9cdd34c46fe8cf4f8f54
Bogdan Neterebskii <bog2dan1@gmail.com>
2016-04-29 11:39:29 +0300

    Wow

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-var timer;
-var minutes, seconds;
-var phase;
-var timeConstants = [5, 2, 3]
+var timer, minutes, seconds;
+var exercise;
+var cycle;
+var cycleCount;
+var pauseTime;
+var timeConstants;
 var interval;
 var display = document.querySelector('#time');
+
 
 function startTimer() {
     interval = setInterval(displayTime, 1000);
@@ -13,11 +16,14 @@ function stopTimer() {
     if (interval != null) clearInterval(interval);
 }
 
-function resetTimer() {
-    phase = 0;
+function updateTimer() {
+    updateSettings();
+    exercise = 0;
+    timer = timeConstants[exercise];
 }
 
 function displayTime() {
+    
     minutes = parseInt(timer / 60, 10);
     seconds = parseInt(timer % 60, 10);
 
@@ -25,41 +31,88 @@ function displayTime() {
     seconds = seconds < 10 ? "0" + seconds : seconds;
 
     display.textContent = minutes + ":" + seconds;
-
-    if (--timer < 0) {
-        if (++phase < timeConstants.length) {
-            console.log(phase);
+    timer--;
+    
+    if (timer < 0) {
+        if (++exercise < timeConstants.length) {
+            console.log(exercise);
             console.log(timeConstants.length);
-            timer = timeConstants[phase];
+            timer = timeConstants[exercise];
         } else {
-            stopTimer();
+            if (++cycle <= cycleCount) {
+                resetCycle();        
+            } else {
+                resetAll();
+                stopTimer();
+            }
         }
     }
+
 }
 
+function resetAll() {
+    resetCycle();
+    cycle = 1;
+}
 
-// function startTimer(duration, display) {
-//     timer = duration;
-//     state = start;
-    
-//     startTimer();
+function resetCycle() {
+    exercise = 0;
+    timer = timeConstants[exercise]; 
+}
 
-//     var minutes, seconds;
-//     interval = setInterval(function () {
-        
-//     }, 1000);
-// }
+function updateSettings() {
+    var pauseValue = document.querySelector('#pauseTime');
+    console.log(pauseValue.valueAsNumber);
+    pauseTime = pauseValue.valueAsNumber;
+
+    var cycleValue = document.querySelector('#cycleCount');
+    console.log(cycleValue.valueAsNumber);
+    cycleCount = cycleValue.valueAsNumber;
+
+    timeConstants = [];
+    var container = document.getElementById("fieldsContainer");
+    var times = container.getElementsByClassName("exerciseTime");
+    for (var i = 0; i < times.length; i++) {
+        timeConstants.push(times[i].valueAsNumber);
+    }
+
+    console.log(timeConstants);
+}
 
 window.onload = function () {
-    phase = 0;
-    timer = timeConstants[phase];
-
-
-    var fiveMinutes = 60 * 5;
-        
+    addField();
     
+    updateSettings();
+
+    resetAll();
 }
 
-function myFunction() {
-    alert('Hello');
+function addField() {
+    var container = document.getElementById("fieldsContainer");
+    var size = container.childElementCount;
+    var div = container.appendChild(document.createElement("div"));
+    div.className = "exercise";
+    div.innerHTML = ++size + ":";
+    
+    var input = document.createElement("input");
+    input.type = "text";
+    input.value = "Exercise " + size;
+    input.id = "exerciseDescription"
+    div.appendChild(input);
+    
+    var input = document.createElement("input");
+    input.type = "number";
+    input.className = "exerciseTime"
+    input.value = 15;
+    input.min = 1;
+    input.max = 65532;
+    div.appendChild(input);
+}
+
+function removeField() {
+    var container = document.getElementById("fieldsContainer");
+    var size = container.childElementCount;
+    if (size > 1) {
+        container.removeChild(container.lastChild);
+    }
 }
